abb162d3e43e46ec58aad8a3e65c985d39bff402
mesay50 <mesayy2017@gmail.com>
2019-08-14 12:25:15 -0400

    first commit

---
+++ src/base/HelloWorld.java
@@ -0,0 +1,14 @@
+package base;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		String first_name="mesay";
+		String student_id="3977ny";
+		int height=172;
+		
+		System.out.println(first_name);
+		System.out.println(student_id);
+		System.out.println(height);
+	}
+
+}
---
+++ src/base/Object.java
@@ -0,0 +1,10 @@
+package base;
+
+public class Object {
+	public static void main(String[] args) {
+		int id=22;
+		String blockNumber="10";
+		System.out.println(id+"/"+blockNumber);
+	}
+
+}
